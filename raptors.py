6a4a8aa9020fc35063b87ef0f5ca78742af64913
Xunzhi Sun <xunzhi.sun@gmail.com>
2012-05-17 10:28:39 -0400

    removed user input, added argument parser for command line using sys.argv. also limited the output type to pdf instead of png

--- raptors.py
+++ raptors.py
@@ -1,26 +1,26 @@
 from reportlab.graphics.shapes import Drawing
 from reportlab.graphics.charts.barcharts import VerticalBarChart
 import csv
+import sys
 
 input_file = 'raps.csv'
-dict_types = {'age': 2, 'g': 3, 'gs': 4, 'mp': 5, 'fg': 6, 'fga': 7, 'fg%': 8, '3p': 9, '3pa': 10, '3p%': 11, 'ft': 12, 'fta': 13, 'ft%': 14, 'orb': 15, 'drb': 16, 'trb': 17, 'ast': 18, 'stl': 19, 'blk': 20, 'tov': 21, 'pf': 22, 'pts': 23}
-user_input = raw_input('category to graph: ')
-#print user_input
-#print dict_types.keys()
+dict_types = {'age': 2, 'g': 3, 'gs': 4, 'mp': 5, 'fg': 6, 'fga': 7, 'fg%': 8, 
+'3p': 9, '3pa': 10, '3p%': 11, 'ft': 12, 'fta': 13, 'ft%': 14, 'orb': 15, 
+'drb': 16, 'trb': 17, 'ast': 18, 'stl': 19, 'blk': 20, 'tov': 21, 'pf': 22, 'pts': 23}
 n_store = []
 v_store = []
 
-if (user_input in dict_types):
-	with open(input_file, 'rb') as f:
-	    reader = csv.reader(f)
-	    for row in reader:
-	        v_store.append (row[dict_types[user_input]])
-	        n_store.append (row[1]) #always append the name
-	
-values = v_store[1:len(v_store)]
+# user sys.argv to passing in the argument since this is just a simple script
+if (len(sys.argv) > 1):
+    if (sys.argv[1] in dict_types):
+	    with open(input_file, 'rb') as f:
+	        reader = csv.reader(f)
+	        for row in reader:
+	            v_store.append (row[dict_types[sys.argv[1]]])
+	            n_store.append (row[1]) #always append the name
 
-#cast each of the items in array to type int
-values = [float(x) for x in values]
+#cast each of the items in array to type float
+values = [float(x) for x in v_store[1:len(v_store)]]
 
 #get max value
 def getMaxValue(array_values = []):
@@ -30,7 +30,7 @@ def getMaxValue(array_values = []):
 			max_value = i
 
 #create a bar graph
-d = Drawing(2000, 1000)
+d = Drawing(1800, 1000) #image size
 
 chart = VerticalBarChart()
 chart.width = 1600
@@ -48,5 +48,5 @@ chart.categoryAxis.categoryNames = n_store[1:len(n_store)]
 chart.valueAxis.valueMin = 0
 
 d.add(chart)
-d.save(fnRoot='test', formats=['png', 'pdf'])
+d.save(fnRoot='test', formats=['pdf'])
 

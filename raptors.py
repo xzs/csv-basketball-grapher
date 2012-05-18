291abe59c64f9b162c359c686caea6099b9dbf6e
Xunzhi Sun <xunzhi.sun@gmail.com>
2012-05-17 23:41:33 -0400

    organized the code a bit. added an error message.

--- raptors.py
+++ raptors.py
@@ -10,45 +10,57 @@ dict_types = {'age': 2, 'g': 3, 'gs': 4, 'mp': 5, 'fg': 6, 'fga': 7, 'fg%': 8,
 n_store = []
 v_store = []
 
-# user sys.argv to passing in the argument since this is just a simple script
-if (len(sys.argv) > 1):
-    if (sys.argv[1] in dict_types):
-	    with open(input_file, 'rb') as f:
-	        reader = csv.reader(f)
-	        for row in reader:
-	            v_store.append (row[dict_types[sys.argv[1]]])
-	            n_store.append (row[1]) #always append the name
-else:
-	sys.exit('Missing argument for: %s' % sys.argv[0])
-
-#cast each of the items in array to type float
-values = [float(x) for x in v_store[1:len(v_store)]]
-
 #get max value
-def getMaxValue(array_values = []):
-	max_value = array_values[0]
-	for i in array_values:
-		if (i > max_value):
-			max_value = i
+def getMaxValue(values = []):
+    max_value = values[0]
+    for i in values:
+        if (i > max_value):
+            max_value = i
 
 #create a bar graph
-d = Drawing(1800, 1000) #image size
+def getPDF(values = []):
+    d = Drawing(1800, 1000) #image size
 
-chart = VerticalBarChart()
-chart.width = 1600
-chart.height = 800
-chart.x = 110
-chart.y = 90
-chart.data = [values]
-chart.groupSpacing = 10
-chart.categoryAxis.labels.angle = 45
-chart.valueAxis.labels.fontSize = 18
-chart.valueAxis.valueMin = 0
-chart.valueAxis.valueMax = getMaxValue(values)
-chart.categoryAxis.labels.fontSize = 24
-chart.categoryAxis.categoryNames = n_store[1:len(n_store)]
-chart.valueAxis.valueMin = 0
+    chart = VerticalBarChart()
+    chart.width = 1600
+    chart.height = 800
+    chart.x = 110
+    chart.y = 90
+    chart.data = [values]
+    chart.groupSpacing = 10
+    chart.categoryAxis.labels.angle = 45
+    chart.valueAxis.labels.fontSize = 18
+    chart.valueAxis.valueMin = 0
+    chart.valueAxis.valueMax = getMaxValue(values)
+    chart.categoryAxis.labels.fontSize = 24
+    chart.categoryAxis.categoryNames = n_store[1:len(n_store)]
+    chart.valueAxis.valueMin = 0
 
-d.add(chart)
-d.save(fnRoot='test', formats=['pdf'])
+    d.add(chart)
+    d.save(fnRoot='test', formats=['pdf'])
+
+# error message
+def getError():
+    print 'You are either using a wrong argument or is missing one, please use one of the following arguments:' 
+    for key in dict_types:
+        sys.stdout.write('%s ' % key)
+    print
+    sys.exit()
+
+# use sys.argv to passing in the argument since this is just a simple script
+if (len(sys.argv) > 1):
+    if (sys.argv[1] in dict_types):
+        with open(input_file, 'rb') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                v_store.append (row[dict_types[sys.argv[1]]])
+                n_store.append (row[1]) #always append the name
+        # convert values into floats so it can be passed into the getPDF function
+        floated_values = [float(x) for x in v_store[1:len(v_store)]]
+        getPDF(floated_values)
+    else:
+        getError()
+else:
+    getError()
+
 
